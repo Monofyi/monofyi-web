131923204d614c8a65855eba2c5b1a3d89084791
Sigmanificient <edhyjox@gmail.com>
2023-04-30 23:59:55 +0200

    :sparkles: Setup colo scheme

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,41 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+
+<style lang="scss">
+
+:root {
+	--palette-white: white;
+	--palette-dark-blue: #060F1C;
+	--palette-orange: #FC7424;
+
+	--palette-dark-grey: #434343;
+	--palette-grey: #828282;
+
+	--bg-dark-mode: var(--palette-dark-blue);
+	--bg-light-mode: var(--palette-white);
+
+	--text-title-dark-mode: var(--palette-white);
+	--text-title-light-mode: var(--palette-grey);
+
+	--text-secondary: var(--palette-grey);
+	--accent: var(--palette-orange);
+
+
+	@media (prefers-color-scheme: dark) {
+		--text-title: var(--text-title-dark-mode);
+		--bg: var(--bg-dark-mode);
+	}
+
+	@media (prefers-color-scheme: light) {
+		--text-title: var(--text-title-light-mode);
+		--bg: var(--bg-light-mode);
+	}
+}
+
+body {
+	color: var(--text-secondary);
+	background: var(--bg);
+}
+
+</style>
