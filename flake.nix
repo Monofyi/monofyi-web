2039a40a8eb31ea528fc396b05a796ebc859c380
Sigmanificient <edhyjox@gmail.com>
2023-04-30 20:33:41 +0200

    :wrench: Add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Monofyi Web";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ nodejs ];
+
+        shellHook = ''
+          echo "node `${pkgs.nodejs}/bin/node --version`"
+        '';
+      };
+    });
+}
